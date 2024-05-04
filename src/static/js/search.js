42920d1f88655357df26d2d29743105d744aab52
mukeshwar9 <mukeshwar1998@gmail.com>
2024-05-04 13:03:06 +0530

    added frontend code

--- src/static/js/search.js
+++ src/static/js/search.js
@@ -1,6 +1,6 @@
-const icon = document.querySelector(".icon");
-const search = document.querySelector(".search");
+// const icon = document.querySelector(".icon");
+// const search = document.querySelector(".search");
 
-icon.onclick = function () {
-  search.classList.toggle("active");
-};
+// icon.onclick = function () {
+//   search.classList.toggle("active");
+// };
