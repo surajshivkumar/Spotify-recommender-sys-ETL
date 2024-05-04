59c8d3f7489df866532e6548969412698d45f2c9
Suraj Shivakumar <surajshivakumar53@gmail.com>
2024-05-04 11:20:58 -0400

    updated config

--- src/app.py
+++ src/app.py
@@ -29,22 +29,14 @@ app = Flask(__name__)
 @app.route("/home",methods=['GET', 'POST'])
 def home():
     songs = caller.getSongs()
-    print(songs.columns)
     songs = songs['Track Name'].unique().tolist()
     return render_template("search.html",songs = songs)
 
-<<<<<<< HEAD
 
 # @app.route("/recommend")
 # def dashboard():
 #     return render_template("dashboard.html")
 
-=======
-# @app.route("/recommend")
-# def dashboard():
-#     return render_template("dashboard.html")
-
->>>>>>> bf4c7c01a4cf52f3ed30bfcf00e8d6be4d5f5a7d
 @app.route("/results",methods=['POST'])
 def results():
     '''
@@ -53,33 +45,13 @@ def results():
     if request.method == 'POST':
         searchTerm = request.form.get('search-term')
         #db search logic below
-<<<<<<< HEAD
         results = ['test']
-=======
-        results = [
-            {
-							'name': 'Blank Space',
-							'imgUrl': 'https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/ea61baa7-9c4b-4f43-805e-81de5fc8aa2b',
-							'artistName': 'Taylor Swift',
-							'timeStamp': '4:33'
-            },
-						{
-							'name': 'Blank Space',
-							'imgUrl': 'https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/ea61baa7-9c4b-4f43-805e-81de5fc8aa2b',
-							'artistName': 'Taylor Swift',
-							'timeStamp': '4:33'
-            },
-						{
-							'name': 'Blank Space',
-							'imgUrl': 'https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/ea61baa7-9c4b-4f43-805e-81de5fc8aa2b',
-							'artistName': 'Taylor Swift',
-							'timeStamp': '4:33'
-            }
-        ]
->>>>>>> bf4c7c01a4cf52f3ed30bfcf00e8d6be4d5f5a7d
         return render_template('dashboard.html', results=results)
 
 
 
 if __name__ == "__main__":
     app.run(debug=True, port=5002,threaded=False)
+    
+
+
