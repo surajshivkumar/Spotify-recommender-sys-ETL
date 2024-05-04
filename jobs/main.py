d11bbf4333d99c774c1a78a982e45b70c74cedaa
Suraj Shivakumar <surajshivakumar53@gmail.com>
2024-05-03 23:23:42 -0400

    job-update

---
+++ jobs/main.py
@@ -0,0 +1,24 @@
+from database import DatabaseManager
+import pandas as pd
+warnings.filterwarnings("ignore")
+
+import json
+
+def load_config(config_path):
+    """Load the ETL configuration file."""
+    with open(config_path, 'r') as file:
+        config = json.load(file)
+    return config
+
+base_dir = '../'
+# config = load_config(base_dir + 'configs/etl_config.json')
+config_db = load_config(base_dir + 'db/db_config.json')
+
+
+def main():
+    recommed_df = pd.read_csv('../data/output/recommendations.csv')
+    dBConfig = config_db["spotify_db"]
+    loader_sql = DatabaseManager(dBConfig)
+    loader_sql.insert_data(dBConfig["table_recommendation"], recommed_df.toPandas().fillna(0).values)
+
+
